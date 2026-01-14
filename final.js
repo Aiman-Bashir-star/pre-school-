0cec5deec5aea5c598f5b8fb1d6689c4c53f8e31
Aiman Bashir <aimanbashir700@gmail.com>
2026-01-14 13:00:05 +0500

    second git commit

---
+++ final.js
@@ -0,0 +1,7 @@
+// // Live update of video feed
+// const videoFeed = document.getElementById('videoFeed');
+
+// setInterval(() => {
+//     const timestamp = new Date().getTime(); // cache buster
+//     videoFeed.src = `/video_feed?${timestamp}`;
+// }, 200);
